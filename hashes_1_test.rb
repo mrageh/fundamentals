5a7dfb192e7d1dadc8b47c607524419dc83685db
Katrina Owen <katrina.owen@gmail.com>
2013-10-08 07:23:58 -0600

    Rewrite basic hashes tests

--- hashes_1_test.rb
+++ hashes_1_test.rb
@@ -77,6 +77,7 @@ class HashesOneTest < Minitest::Test
   # So you can look up the name in the phone book and
   # get back the phone number.
   def test_the_phone_book
+    skip
     phone_book = {
       "Howe, Joanie" => "559-989-3056",
       "Tremblay, Agustin" => "755-536-0289",
@@ -102,7 +103,32 @@ class HashesOneTest < Minitest::Test
     # Ruben Feeney
   end
 
+  def test_waiting_list_at_the_post_office
+    skip
+    people = {
+      289 => "August Glover",
+      291 => "Gordon Johnston",
+      292 => "Leila Little",
+      295 => "Jefferey Barrows",
+      296 => "Nils Gorczany",
+      297 => "Imelda Leffler",
+      299 => "Merlin Wyman",
+      305 => "Aaron Mills",
+      306 => "Mark Muller",
+      309 => "Jackson Crooks"
+    }
+    # Write the assertions to prove that you're
+    # able to look up the following people by
+    # their queuing number.
+    #
+    # Leila Little
+    # Mark Muller
+    # Aaron Mills
+    # August Glover
+  end
+
   def test_birthdays_in_a_school_class
+    skip
     fourth_grade = {
       "Macy" => "December 2",
       "Coy" => "March 2",
@@ -121,6 +147,24 @@ class HashesOneTest < Minitest::Test
     # What is Raquel's birthday?
   end
 
+  # In Scrabble we know what the letter is, but not
+  # the score. We can look up the score in a hash.
+  def test_scrabble_scores
+    skip
+    scores = {
+      "A"=>1, "B"=>3, "C"=>3, "D"=>2, "E"=>1, "F"=>4,
+      "G"=>2, "H"=>4, "I"=>1, "J"=>8, "K"=>5, "L"=>1,
+      "M"=>3, "N"=>1, "O"=>1, "P"=>3, "Q"=>10, "R"=>1,
+      "S"=>1, "T"=>1, "U"=>1, "V"=>4, "W"=>4, "X"=>8,
+      "Y"=>4, "Z"=>10
+    }
+
+    # How much is the letter Q worth?
+    # How about J?
+    # And F?
+    # Each time, prove that you're right with an assertion.
+  end
+
   # The Heller family has many dogs.
   # You know the dogs names, but since
   # you're quite ignorant about breeds of
@@ -133,6 +177,7 @@ class HashesOneTest < Minitest::Test
   # What breed is she?
   # Answer: poodle.
   def test_add_more_pets
+    skip
     pets = {
       'Rex' => 'bulldog',
       'Ladybug' => 'poodle',
@@ -162,28 +207,138 @@ class HashesOneTest < Minitest::Test
     assert_equal 'poodle', pets['Diamond']
   end
 
-  # Keys don't have to be strings.
-  def test_wines_by_year
-    wines = {
-      2001 => 'Chateauneuf du Pape'
-      # more wines here
+  def test_hashes_and_arrays_can_seem_kind_of_similar
+    skip
+    # This is an array
+    line_of_people = ["Alice", "Bob", "Charlie", "Dave", "Eve"]
+
+    # This is how we find the person who is in position 1:
+    # line_of_people[1]. Let's make sure we found the right
+    # one:
+    assert_equal "Bob", line_of_people[1]
+
+    # Your turn.
+    # Who is at position 3?
+    # Who is at position 5?
+
+    # This is how you add a new person to the end of the line.
+    line_of_people << "Fred"
+
+    # Your turn.
+    # Add George to the line.
+    # Add Harriet to the line.
+
+    # Who is at position 5 now? Write an
+    # assertion to prove that you are right.
+
+
+    # Now we're done with the array.
+    # This is a hash with numeric keys
+    crowd_of_people = {
+      0 => "Alice",
+      1 => "Bob",
+      2 => "Charlie",
+      3 => "Dave",
+      4 => "Eve"
     }
-    # add simple lookup tests / assertions
+
+    # Notice that people aren't lined up. It's more like
+    # they're milling around the room, and they've been given
+    # a number, like at a restaurant where they'll call your
+    # number when your order is ready.
+
+    # This is how we find the person with number 1:
+    # crowd_of_people[1].
+    # Let's make sure we found the right one:
+    assert_equal "Bob", crowd_of_people[1]
+
+    # This is how you add more people to the hash:
+    crowd_of_people[5] = "Fred"
+
+    # Look. We can give them any number.
+    crowd_of_people[42] = "George"
+    crowd_of_people[-1] = "Harriet"
+
+    # Your turn.
+    # Add Isabelle to the crowd.
+    # Add Johnathon to the crowd.
+
+
+    # Who is at position 4?
+    # Who is at position 5?
+    # Who is at position 6?
+    # Who is at position 42?
   end
 
-  def test_waiting_list_at_the_post_office
-    people = {
-      289 => "August Glover",
-      # 290 accidentally got thrown in the bin
-      291 => "Gordon Johnston"
-      # add more people
+  def test_find_all_the_same_breeds
+    skip
+    pets = {
+      'Rex' => 'bulldog',
+      'Ladybug' => 'poodle',
+      'Scotch' => 'Labrador',
+      'Ruby' => 'Labrador',
+      'Diamond' => 'poodle',
+      'Bones' => 'bulldog'
     }
-    # add simple lookup tests / assertions
+
+    # We can only look things up by key, so if we want
+    # to do anything by the values, we have to loop through
+    # everything.
+    #
+    # This is how to loop through to find all the poodles:
+    poodles = []
+    pets.each do |name, breed|
+      if breed == 'poodle'
+        poodles << name
+      end
+    end
+    assert_equal ['Ladybug', 'Diamond'], poodles
+
+    # Your turn.
+    # Find all the Labradors.
+    # Find all the bulldogs.
   end
 
-  # TODO: Add looping examples, for example
-  # Find all the names of the poodles.
-  # Find all the wines from 2003.
-  # Find everyone with a birthday in March.
+  def test_books_by_publishing_year
+    skip
+    books = {
+      'Dragonrider' => 1968,
+      'Dune' => 1965,
+      'Foundation' => 1951,
+      'A Wrinkle in Time' => 1962,
+      'The Hitchhiker\'s Guide to the Galaxy' => 1979,
+      'Neuromancer' => 1984,
+      'A Clockwork Orange' => 1962,
+      '2001: A Space Odyssey' => 1968,
+      'Fahrenheit 451' => 1954,
+      'The Puppet Masters' => 1951,
+      'Cryptonomicon' => 1999,
+    }
+
+    # Find all the books that were published in 1968.
+    # Write an assertion to prove that you're right.
+    # Now do the same for 1962.
+    # Now find all the books published in 1951.
+  end
+
+  def test_birthdays_in_a_school_class
+    skip
+    fourth_grade = {
+      "Macy" => "December 2",
+      "Coy" => "March 2",
+      "Alf" => "May 12",
+      "Rebecca" => "October 16",
+      "Corbin" => "May 18",
+      "Raquel" => "July 24",
+      "Delilah" => "October 31",
+      "Breana" => "May 30",
+      "Jamey" => "November 8",
+      "Charlie" => "October 26"
+    }
+
+    # Find everyone whose birthday is in May.
+    # Find everyone who has a birthday in October.
+  end
+
 end
 
