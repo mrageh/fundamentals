989825c7453a6d500f33cf3f656f4f499bbc7897
Adam89 <Adamm1@aston.ac.uk>
2013-12-23 12:19:08 -0700

    finished command-query section

--- command-query/student.rb
+++ command-query/student.rb
@@ -2,13 +2,20 @@ class Student
   attr_reader :grade
 
   def initialize
-    @times = 0
-    @grades = ['C','B','A']
-    @grade = @grades[@times]
+    @times   = 2
+    @grades  = ['A', 'B', 'C', 'D', 'F']
+    @grade   = @grades[@times]
   end
 
   def study
+    @times -= 1
+    @times = 0 if @times < 0
+    @grade = @grades[@times]
+  end
+
+  def slack_off
     @times += 1
+    @times = 4 if @times > 4
     @grade = @grades[@times]
   end
 end
